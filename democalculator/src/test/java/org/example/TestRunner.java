2fd6855721b1c8f351195547de550dac35cb5003
CignitiArun <87745164+CignitiArun@users.noreply.github.com>
2023-05-12 13:25:38 +0530

    Update TestRunner.java

--- democalculator/src/test/java/org/example/TestRunner.java
+++ democalculator/src/test/java/org/example/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(
-        features = "src/test/resources",
+        features = "src/test/resources/features",
         glue = {"org.example.stepdefs"},
         plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"},
         tags = {"@smoke"},
