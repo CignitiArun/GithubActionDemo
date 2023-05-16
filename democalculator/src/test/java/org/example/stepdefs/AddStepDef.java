3d417b6d3090aaf20b35d1a6a393286a5eae778e
CignitiArun <87745164+CignitiArun@users.noreply.github.com>
2023-05-16 18:27:13 +0530

    Update AddStepDef.java

--- democalculator/src/test/java/org/example/stepdefs/AddStepDef.java
+++ democalculator/src/test/java/org/example/stepdefs/AddStepDef.java
@@ -14,5 +14,19 @@ public class AddStepDef {
     public void iAddAndTheADDServiceShouldReturnMe(int arg0, int arg1) {
         world.addServiceResponse = world.calculatorSoapIns.add(arg0, arg1);
     }
+    
+    @Given("I get the data from file {string} from {int} for addition")
+	public void theDataInFile(String fileName, int index) throws Exception {
+		File file = new File(getClass().getClassLoader().getResource(fileName.replaceAll("\"", "")).getFile());
+		String jsonData = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+		ObjectMapper mapper = new ObjectMapper();
+		List<Map<String, Integer>> data = mapper.readValue(jsonData, List.class);
+		world.exampleData = data;
+		Map<String, Integer> dataMap = world.exampleData.get(index);
+		int num1 = dataMap.get("num1");
+		int num2 = dataMap.get("num2");
+		world.addServiceResponse = world.calculatorSoapIns.add(num1, num2);
+
+	}
 
 }
