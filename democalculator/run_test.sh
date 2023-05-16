9637d14fa937c7936b5394b87c611ce449cf1f5e
CignitiArun <87745164+CignitiArun@users.noreply.github.com>
2023-05-16 16:58:16 +0530

    Update run_test.sh

--- democalculator/run_test.sh
+++ democalculator/run_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-count=0
-while IFS=, read -r name code; do
-  # do something...
-  [[ "$code" == *","* ]] && echo "$name $code" && ((++count))
-done < input.csv; \
-echo ">> we found ${count} bad entries"
+while IFS="," read -r rec_column1 rec_column2
+do
+  echo "Displaying Record-$rec_column1"
+  echo "Quantity: $rec_column2"
+
+  mvn clean test verify -Dcucumber.options="--tags @$rec_column1" -Dglue=org.example.stepdefs -Dplugin=json:target/cucumber-reports/Cucumber.json -Ddataproviderthreadcount=3 
+done < <(tail -n +2 input.csv)
