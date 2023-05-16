c3117e6557b040f916b9fa2c6a889eb3189e1e27
CignitiArun <87745164+CignitiArun@users.noreply.github.com>
2023-05-16 16:22:14 +0530

    Update run_test.sh

--- democalculator/run_test.sh
+++ democalculator/run_test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-mvn clean test verify -Dcucumber.options="--tags @AddService,@SubtractServiceExample" -Dglue=org.example.stepdefs -Dplugin=json:target/cucumber-reports/Cucumber.json -Ddataproviderthreadcount=3 
+while IFS="," read -r rec_column1 rec_column2
+do
+  echo "Displaying Record-$rec_column1"
+  echo "Quantity: $rec_column2"
+  
+  mvn clean test verify -Dcucumber.options="--tags @$rec_column1" -Dglue=org.example.stepdefs -Dplugin=json:target/cucumber-reports/Cucumber.json -Ddataproviderthreadcount=3 
+done < <(tail -n +2 input.csv)
